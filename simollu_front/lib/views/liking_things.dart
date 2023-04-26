52b6a7416d804cb95d130519e8ba14ecb0559fed
chloe73 <esom735@gmail.com>
2023-04-26 16:59:23 +0900

    [FE] feat: LikingThings button class 생성

--- simollu_front/lib/views/liking_things.dart
+++ simollu_front/lib/views/liking_things.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simollu_front/views/liking_things_button.dart';
 
 class LikingThings extends StatelessWidget {
   const LikingThings({Key? key}) : super(key: key);
@@ -8,34 +9,19 @@ class LikingThings extends StatelessWidget {
     return MaterialApp(
         home: Container(
           color: Colors.white,
-          child: Center(
-            child: Container(
-              margin: EdgeInsets.only(top: 170), // marginTop 설정
-              // height: 200.0, // 세로 크기
-              child: Column(
-                children: [
-                  Text(
-                    "기다리면서",
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 50,
-                      fontFamily: 'Roboto',
-                      fontWeight: FontWeight.bold,
-                      fontStyle: FontStyle.normal,
-                      letterSpacing: 0,
-                      wordSpacing: 0,
-                      height: 1.0,
-                      shadows: [],
-                      decoration: TextDecoration.none,
-                    ),
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+          height: double.infinity,
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.only(top: 170), // marginTop 설정
+                  // height: 200.0, // 세로 크기
+                  child: Column(
                     children: [
-                      Text(
-                        "뭐하고 ",
+                      const Text(
+                        "기다리면서",
                         style: TextStyle(
-                          color: Colors.yellow,
+                          color: Colors.black,
                           fontSize: 50,
                           fontFamily: 'Roboto',
                           fontWeight: FontWeight.bold,
@@ -47,62 +33,29 @@ class LikingThings extends StatelessWidget {
                           decoration: TextDecoration.none,
                         ),
                       ),
-                      Text(
-                          "싶어?",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 50,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.normal,
-                            letterSpacing: 0,
-                            wordSpacing: 0,
-                            height: 1.0,
-                            shadows: [],
-                            decoration: TextDecoration.none,
-                          )
-                      ),
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 20),
-                    child: Image.asset('assets/liking-image.png'),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(left: 21.0, right: 21.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        OutlinedButton(
-                          onPressed: () {
-                            print('독서 클릭!!!!!!!!');
-                          },
-                          child: Text(
-                              '독서',
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "뭐하고 ",
+                            style: TextStyle(
+                              color: Color(0xFFFFD200),
+                              fontSize: 50,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.normal,
+                              letterSpacing: 0,
+                              wordSpacing: 0,
+                              height: 1.0,
+                              shadows: [],
+                              decoration: TextDecoration.none,
+                            ),
+                          ),
+                          Text(
+                              "싶어?",
                               style: TextStyle(
                                 color: Colors.black,
-                                fontFamily: 'Roboto',
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.normal,
-                                letterSpacing: 0,
-                                wordSpacing: 0,
-                                height: 1.0,
-                                shadows: [],
-                                decoration: TextDecoration.none,
-                              )),
-                          style: OutlinedButton.styleFrom(
-                              side: BorderSide( // 테두리 바꾸는 속성
-                                color: Colors.black54,
-                                width: 1.0,
-                              )),
-                        ),
-                        OutlinedButton(
-                          onPressed: () {
-                            print('걷기 클릭!!!!!!!!');
-                          },
-                          child: Text('걷기',
-                              style: TextStyle(
-                                color: Colors.black,
+                                fontSize: 50,
                                 fontFamily: 'Roboto',
                                 fontWeight: FontWeight.bold,
                                 fontStyle: FontStyle.normal,
@@ -113,136 +66,107 @@ class LikingThings extends StatelessWidget {
                                 decoration: TextDecoration.none,
                               )
                           ),
-                          style: OutlinedButton.styleFrom(
-                              side: BorderSide( // 테두리 바꾸는 속성
-                                color: Colors.black54,
-                                width: 1.0,
-                              )),
+                        ],
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 20),
+                        child: Image.asset('assets/liking-image.png'),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 21.0, right: 21.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            CustomButton(
+                              text: '독서',
+                              onPressed: () {
+                                print('독서 클릭!!!!!!!!');
+                              },
+                            ),
+                            CustomButton(
+                              text: '걷기',
+                              onPressed: () {
+                                print('걷기 클릭!!!!!!!!');
+                              },
+                            ),
+                            CustomButton(
+                              text: '사진',
+                              onPressed: () {
+                                print('사진 클릭!!!!!!!!');
+                              },
+                            ),
+                            CustomButton(
+                              text: '쇼핑',
+                              onPressed: () {
+                                print('쇼핑 클릭!!!!!!!!');
+                              },
+                            ),
+                          ],
                         ),
-                        OutlinedButton(
-                          onPressed: () {
-                            print('사진 클릭!!!!!!!!');
-                          },
-                          child: Text('사진',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Roboto',
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.normal,
-                                letterSpacing: 0,
-                                wordSpacing: 0,
-                                height: 1.0,
-                                shadows: [],
-                                decoration: TextDecoration.none,
-                              )),
-                          style: OutlinedButton.styleFrom(
-                              side: BorderSide( // 테두리 바꾸는 속성
-                                color: Colors.black54,
-                                width: 1.0,
-                              )),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(left: 35.0, right: 35.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            CustomButton(
+                              text: '노래',
+                              onPressed: () {
+                                print('노래 클릭!!!!!!!!');
+                              },
+                            ),
+                            CustomButton(
+                              text: '휴식',
+                              onPressed: () {
+                                print('휴식 클릭!!!!!!!!');
+                              },
+                            ),
+                            CustomButton(
+                              text: '오락 & 게임',
+                              onPressed: () {
+                                print('오락 & 게임 클릭!!!!!!!!');
+                              },
+                            ),
+                          ],
                         ),
-                        OutlinedButton(
-                          onPressed: () {
-                            print('쇼핑 클릭!!!!!!!!');
-                          },
-                          child: Text('쇼핑',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Roboto',
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.normal,
-                                letterSpacing: 0,
-                                wordSpacing: 0,
-                                height: 1.0,
-                                shadows: [],
-                                decoration: TextDecoration.none,
-                              )),
-                          style: OutlinedButton.styleFrom(
-                              side: BorderSide( // 테두리 바꾸는 속성
-                                color: Colors.black54,
-                                width: 1.0,
-                              )),
-                        ),
-                      ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: EdgeInsets.only(top: 160),
+                  padding: EdgeInsets.only(left: 10, right: 10),
+                  child: Container(
+                    width: double.infinity,
+                    height: 45,
+                    child: ElevatedButton(
+                      onPressed: () {  },
+                      child: const Text(
+                          '완료',
+                          style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.normal,
+                          letterSpacing: 0,
+                          wordSpacing: 0,
+                          height: 1.0,
+                          shadows: [],
+                          decoration: TextDecoration.none,
+                          )),
+                      style: ElevatedButton.styleFrom(
+                        // backgroundColor: Colors.yellow
+                        backgroundColor: Color(0xFFFFD200)
+                      ),
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(left: 35.0, right: 35.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        OutlinedButton(
-                          onPressed: () {
-                            print('노래 클릭!!!!!!!!');
-                          },
-                          child: Text('노래',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Roboto',
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.normal,
-                                letterSpacing: 0,
-                                wordSpacing: 0,
-                                height: 1.0,
-                                shadows: [],
-                                decoration: TextDecoration.none,
-                              )),
-                          style: OutlinedButton.styleFrom(
-                              side: BorderSide( // 테두리 바꾸는 속성
-                                color: Colors.black54,
-                                width: 1.0,
-                              )),
-                        ),
-                        OutlinedButton(
-                          onPressed: () {
-                            print('휴식 클릭!!!!!!!!');
-                          },
-                          child: Text('휴식',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Roboto',
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.normal,
-                                letterSpacing: 0,
-                                wordSpacing: 0,
-                                height: 1.0,
-                                shadows: [],
-                                decoration: TextDecoration.none,
-                              )),
-                          style: OutlinedButton.styleFrom(
-                              side: BorderSide( // 테두리 바꾸는 속성
-                                color: Colors.black54,
-                                width: 1.0,
-                              )),
-                        ),
-                        OutlinedButton(
-                          onPressed: () {
-                            print('오락&게임 클릭!!!!!!!!');
-                          },
-                          child: Text('오락 & 게임',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontFamily: 'Roboto',
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.normal,
-                                letterSpacing: 0,
-                                wordSpacing: 0,
-                                height: 1.0,
-                                shadows: [],
-                                decoration: TextDecoration.none,
-                              )),
-                          style: OutlinedButton.styleFrom(
-                              side: BorderSide( // 테두리 바꾸는 속성
-                                color: Colors.black54,
-                                width: 1.0,
-                              )),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
+                ),
+                ),
+            ],
           ),
         )
     );
