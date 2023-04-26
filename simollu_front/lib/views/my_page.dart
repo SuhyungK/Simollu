264da0c9338167b79e35eba819e65b64906a5a2e
고태진 <thereisnotruth12@gmail.com>
2023-04-26 14:36:03 +0900

    [FE] feat: 마이 페이지 구현

---
+++ simollu_front/lib/views/my_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/footer.dart';
+
+class MyPage extends StatefulWidget {
+  const MyPage({super.key});
+
+  @override
+  State<MyPage> createState() => MyPageState();
+}
+
+class MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 50,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Center(
+          child: Text(
+            '마이 페이지',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: SizedBox(
+        child: Column(
+          children: [
+            Container(
+              height: 135,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(50),
+                ),
+                color: Color(0xFFFFD200),
+              ),
+            ),
+            Container(
+              height: 120,
+              decoration: BoxDecoration(
+                color: Color(0xFFFFD200),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Center(
+                  child: Container(
+                    height: 100,
+                    width: 320,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.3),
+                          spreadRadius: 1.5,
+                          blurRadius: 5,
+                          offset: Offset(0, 3),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      // Footer
+      bottomNavigationBar: Footer(),
+    );
+  }
+}
