464f1415da0666bd8494c4ce63d473075508a010
고태진 <thereisnotruth12@gmail.com>
2023-04-27 14:43:52 +0900

    [FE] feat: 마이 페이지 구현

--- simollu_front/lib/views/my_page.dart
+++ simollu_front/lib/views/my_page.dart
@@ -38,95 +38,184 @@ class MyPageState extends State<MyPage> with SingleTickerProviderStateMixin {
           ),
         ),
       ),
-      body: SizedBox(
-        child: Column(
-          children: [
-            // 노란색 박스
-            // 프로필 이미지, 닉네임, 화살표 버튼
-            Container(
-              height: 135,
+      body: Column(
+        children: [
+          // 노란색 박스
+          // 프로필 이미지, 닉네임, 화살표 버튼
+          Container(
+            height: 135,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(50),
+              ),
+              color: Color(0xFFFFD200),
+            ),
+          ),
+          // 흰색 박스
+          // 포크 수, 관심 매장 수, 작성 리뷰 수
+          Container(
+            height: 120,
+            decoration: BoxDecoration(
+              color: Color(0xFFFFD200),
+            ),
+            child: Container(
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(50),
+                  topLeft: Radius.circular(50),
                 ),
-                color: Color(0xFFFFD200),
+                color: Colors.white,
               ),
-            ),
-            // 흰색 박스
-            // 포크 수, 관심 매장 수, 작성 리뷰 수
-            Container(
-              height: 120,
-              decoration: BoxDecoration(
-                color: Color(0xFFFFD200),
-              ),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50),
-                  ),
-                  color: Colors.white,
-                ),
-                child: Center(
-                  child: Container(
-                    height: 100,
-                    width: 320,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20),
-                      ),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.3),
-                          spreadRadius: 1,
-                          blurRadius: 10,
-                          offset: Offset(0, 6),
-                        )
-                      ],
+              child: Center(
+                child: Container(
+                  height: 100,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20),
                     ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.3),
+                        spreadRadius: 1,
+                        blurRadius: 10,
+                        offset: Offset(0, 6),
+                      )
+                    ],
                   ),
                 ),
               ),
             ),
-            // 리스트
-            Column(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    print('웨이팅 기록');
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(30),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Color(0xFFDDDDDD),
-                          width: 1,
-                        ),
+          ),
+          // 리스트
+          Column(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  print('웨이팅 기록');
+                },
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Color(0xFFDDDDDD),
+                        width: 1,
                       ),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: const [
-                        Text(
-                          '웨이팅 기록',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 20,
-                          ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        '웨이팅 기록',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Icon(Icons.arrow_forward_ios_outlined),
-                      ],
-                    ),
+                      ),
+                      Icon(Icons.arrow_forward_ios_outlined),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ],
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('최근 본 식당');
+                },
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Color(0xFFDDDDDD),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        '최근 본 식당',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios_outlined),
+                    ],
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  print('회원 탈퇴');
+                },
+                child: Container(
+                  padding: EdgeInsets.all(30),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Color(0xFFDDDDDD),
+                        width: 1,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        '회원 탈퇴',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.arrow_forward_ios_outlined),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: TextButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                alignment: Alignment.center,
+                backgroundColor: MaterialStateProperty.all(
+                  Color(0xFFFFD200),
+                ),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  '로그아웃',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              )),
         ),
       ),
-      // Footer
     );
   }
 }
