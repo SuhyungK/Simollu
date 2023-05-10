9f0d9b88230bb68b0c3d8b2ae9bfd1c00579cfe1
고태진 <thereisnotruth12@gmail.com>
2023-05-10 12:50:46 +0900

    [FE] feaet: 장소 클래스 구현

---
+++ simollu_front/lib/models/place.dart
@@ -0,0 +1,15 @@
+class Place {
+  final String id;
+  final String name;
+  final String address;
+  final double lat;
+  final double lng;
+
+  Place({
+    required this.id,
+    required this.name,
+    required this.address,
+    required this.lat,
+    required this.lng,
+  });
+}
