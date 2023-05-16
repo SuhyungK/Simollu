568854348436d0c14b132dddde905a60f5948d5a
고태진 <thereisnotruth12@gmail.com>
2023-05-16 17:34:15 +0900

    [FE] feat: 메인 페이지 식당 리스트에 주소와 거리 표시

--- simollu_front/lib/models/restaurant_model.dart
+++ simollu_front/lib/models/restaurant_model.dart
@@ -4,18 +4,25 @@ class RestaurantModel {
   final int restaurantRating;
   final int restaurantWaitingTime;
   final String restaurantImage;
+  final String restaurantAddress;
+  final double distance;
 
-  RestaurantModel(
-      {required this.restaurantSeq,
-      required this.restaurantName,
-      required this.restaurantRating,
-      required this.restaurantWaitingTime,
-      required this.restaurantImage});
+  RestaurantModel({
+    required this.restaurantSeq,
+    required this.restaurantName,
+    required this.restaurantRating,
+    required this.restaurantWaitingTime,
+    required this.restaurantImage,
+    required this.restaurantAddress,
+    required this.distance,
+  });
 
   RestaurantModel.fromJSON(Map<String, dynamic> json)
       : restaurantSeq = json['restaurantSeq'] ?? '',
         restaurantName = json['restaurantName'] ?? '',
         restaurantRating = json['restaurantRating'] ?? 0,
         restaurantWaitingTime = json['restaurantWaitingTime'] ?? 0,
-        restaurantImage = json['restaurantImage'] ?? '';
+        restaurantImage = json['restaurantImage'] ?? '',
+        restaurantAddress = json['restaurantAddress'] ?? '',
+        distance = json['distance'] ?? 0;
 }
