5283b160d0e0f40f5748b01adf1e335fc559b4a4
cladren123 <gold3987@naver.com>
2023-05-17 13:46:16 +0900

    [BE] feat alert service upload

---
+++ Alert/src/main/java/com/simollu/Alert/model/dto/NotificationRequestDto.java
@@ -0,0 +1,15 @@
+package com.simollu.Alert.model.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NotificationRequestDto {
+    private String targetUserSeq;
+    private String title;
+    private String body;
+
+}
