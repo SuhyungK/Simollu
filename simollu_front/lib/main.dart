9f61d342c522ac66360f10234d42820ab09a34ba
SuhyungK <dook4966@gmail.com>
2023-05-18 08:15:35 +0900

    Merge branch 'dev-fe' of https://lab.ssafy.com/s08-final/S08P31A701 into fe/feat/review_김수형
    # Conflicts:
    #	simollu_front/lib/main.dart
    #	simollu_front/pubspec.yaml

--- simollu_front/lib/main.dart
+++ simollu_front/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
-import 'package:simollu_front/utils/fcmSetting.dart';
+// import 'package:simollu_front/utils/fcmSetting.dart';
 import 'package:simollu_front/viewmodels/main_view_model.dart';
 import 'package:simollu_front/viewmodels/map_view_model.dart';
 import 'package:simollu_front/viewmodels/search_view_model.dart';
@@ -12,7 +12,7 @@ import 'package:simollu_front/root.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  String? firebaseToken = await fcmSetting();
+  // String? firebaseToken = await fcmSetting();
   await dotenv.load(fileName: ".env"); // 추가
   runApp(
     MyApp(),
