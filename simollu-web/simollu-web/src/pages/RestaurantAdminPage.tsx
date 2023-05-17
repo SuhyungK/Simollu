e0423c22feb1b18e0de9f45d50dc626575f06faa
esom735 <esom735@gmail.com>
2023-05-17 09:01:38 +0900

    [FE] feat: simollu-web 세팅

---
+++ simollu-web/simollu-web/src/pages/RestaurantAdminPage.tsx
@@ -0,0 +1,11 @@
+import "../styles/main.scss";
+
+export default function RestaurantAdminPage() {
+  return (
+    <div className="restaurant-admin-page">
+      <div className="restaurant-admin-page__logo">
+        <div className="restaurant-admin-page__text">동래정 선릉직영점</div>
+      </div>
+    </div>
+  );
+}
