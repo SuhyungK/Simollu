beeee36a1441e4b2dd7feaad6826f9d47aa88568
cladren123 <gold3987@naver.com>
2023-05-18 13:50:24 +0900

    api gateway cors 설정

--- ApigatewayService/src/main/java/com/simollu/ApigatewayService/config/CorsGlobalConfiguration.java
+++ ApigatewayService/src/main/java/com/simollu/ApigatewayService/config/CorsGlobalConfiguration.java
@@ -17,28 +17,28 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 @EnableWebFlux
 public class CorsGlobalConfiguration implements WebFluxConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry corsRegistry) {
-//        corsRegistry.addMapping("/**")
-//                .allowedOriginPatterns("http://localhost:3000", "https://simollu.com")
-//                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-//                .allowedHeaders("*")
-//                .allowCredentials(true)
-//                .maxAge(3600);
-//    }
-
-
-    @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public CorsConfiguration corsConfiguration(
-            RoutePredicateHandlerMapping routePredicateHandlerMapping) {
-        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
-
-        Arrays.asList(HttpMethod.OPTIONS, HttpMethod.PUT, HttpMethod.GET, HttpMethod.DELETE, HttpMethod.POST) .forEach(m -> corsConfiguration.addAllowedMethod(m));
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedMethod(HttpMethod.POST);
-        routePredicateHandlerMapping.setCorsConfigurations(new HashMap<String, CorsConfiguration>() {{ put("/**", corsConfiguration); }});
-        return corsConfiguration;
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOriginPatterns("*","http://localhost:3000", "https://simollu.com")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
+
+
+//    @Bean
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public CorsConfiguration corsConfiguration(
+//            RoutePredicateHandlerMapping routePredicateHandlerMapping) {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+//
+//        Arrays.asList(HttpMethod.OPTIONS, HttpMethod.PUT, HttpMethod.GET, HttpMethod.DELETE, HttpMethod.POST) .forEach(m -> corsConfiguration.addAllowedMethod(m));
+//        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+//        routePredicateHandlerMapping.setCorsConfigurations(new HashMap<String, CorsConfiguration>() {{ put("/**", corsConfiguration); }});
+//        return corsConfiguration;
+//    }
 }
 
