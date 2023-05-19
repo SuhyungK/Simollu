f0d00add3a4786a5cd71777759105e205c5d1758
임혜은 <calicedev@gmail.com>
2023-05-19 16:18:19 +0900

    [BE] : ELK 업로드

--- UserService/src/main/java/com/simollu/UserService/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.simollu.UserService.repository;
-
-import com.simollu.UserService.entity.Authority;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
